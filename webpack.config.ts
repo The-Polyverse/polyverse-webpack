8cc7a53e0fc168c70aadc2a6b6e09f53eae4904f
Ivgeni <ivgenis@gmail.com>
2023-10-15 22:11:10 +0000

    Basic Webpack Setup

---
+++ webpack.config.ts
@@ -0,0 +1,26 @@
+import path from 'path';
+import { Configuration } from 'webpack';
+
+const config: Configuration = {
+  entry: {
+    index: path.resolve(__dirname, 'src', 'index.ts'),
+  },
+  target: 'node', // in order to ignore built-in modules like path, fs, etc.
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: [/node_modules/],
+      },
+    ],
+  },
+};
