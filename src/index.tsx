9a95c4f5aeb0c80d98eae9cc353f0b6e51a10acb
Ivgeni <ivgenis@gmail.com>
2023-10-15 23:38:37 +0000

    Fixes: Yarn version & React best practices.

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,5 @@ import { createRoot } from "react-dom/client";
 console.log("Hello World!");
 
 // Render your React component instead
-const root = createRoot(document.body);
+const root = createRoot(document.getElementById("root")!);
 root.render(<h1>Hello, world</h1>);
