1ece3ee3a4e2864fb58a673b0c2e25be3b03be84
Ivgeni <ivgenis@gmail.com>
2023-10-16 20:45:01 +0000

    client and server entries for webpack.

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,31 +1,54 @@
 import path from 'path';
 import { Configuration } from 'webpack';
 
-const server = (env: any, argv: any): Configuration => {
-  return {
-    name: 'client',
-    entry: {
-      index: './src/index.tsx',
-    },
-    target: 'web', // in order to ignore built-in modules like path, fs, etc.
-    mode: 'development',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'index.js',
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          loader: 'ts-loader',
-          exclude: [/node_modules/],
-        },
-      ],
-    },
-  }
-};
+const client = (env: any, argv: any): Configuration => ({
+  name: 'client',
+  entry: {
+    index: './src/index.tsx',
+  },
+  target: 'web', // in order to ignore built-in modules like path, fs, etc.
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: [/node_modules/],
+      },
+    ],
+  },
+});
 
-export default [server];
+const server: (env: any, argv: any) => Configuration = (env, argv) => ({
+  name: 'server',
+  entry: {
+    index: './src/server/index.tsx',
+  },
+  target: 'node', // in order to ignore built-in modules like path, fs, etc.
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'server.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: [/node_modules/],
+      },
+    ],
+  },
+});
+
+export default [client, server];
