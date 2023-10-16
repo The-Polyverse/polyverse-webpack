2cc303984ec7772a07557663e0eb6a09e8791be4
Ivgeni <ivgenis@gmail.com>
2023-10-16 20:48:49 +0000

    basic express app running

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -1,1 +1,14 @@
+import express from "express";
+
+
 console.log("Hello World!");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
