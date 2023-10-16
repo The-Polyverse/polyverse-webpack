b65b0ba09300afd31ad9891a8800bae7969ecd10
Ivgeni <ivgenis@gmail.com>
2023-10-16 20:32:24 +0000

    expand webpack.config for multiple targets

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,28 +1,31 @@
 import path from 'path';
 import { Configuration } from 'webpack';
 
-const config: Configuration = {
-  entry: {
-    index: './src/index.tsx',
-  },
-  target: 'node', // in order to ignore built-in modules like path, fs, etc.
-  mode: 'development',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        exclude: [/node_modules/],
-      },
-    ],
-  },
+const server = (env: any, argv: any): Configuration => {
+  return {
+    name: 'client',
+    entry: {
+      index: './src/index.tsx',
+    },
+    target: 'web', // in order to ignore built-in modules like path, fs, etc.
+    mode: 'development',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index.js',
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          exclude: [/node_modules/],
+        },
+      ],
+    },
+  }
 };
 
-export default config;
+export default [server];
