dcb0f4e04cafeed9a615ae9880755d4fc019e4fc
Ivgeni <ivgenis@gmail.com>
2023-10-17 20:34:12 -0400

    Create document.tsx

---
+++ src/components/document.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Document() {
+  return (
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Webpack App</title>
+      </head>
+      <body>
+        <div id="root">Hello World!!</div>
+      </body>
+    </html>
+  );
+}
