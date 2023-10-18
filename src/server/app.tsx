5a2f29a1069a30c7e0c8a45786a4949815c74c4e
Ivgeni <ivgenis@gmail.com>
2023-10-17 20:50:07 -0400

    Update app.tsx

--- src/server/app.tsx
+++ src/server/app.tsx
@@ -1,23 +1,11 @@
 import express from "express";
+import { renderToString } from "react-dom/server";
+import { createElement } from 'react';
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(`<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Webpack App!</title>
-  </head>
-  <body>
-    <div id="root">Hello World!!</div>
-    <script src="https://${
-        process.env.CODESPACE_NAME
-    }-8080.${
-        process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN
-    }/index.js"></script>
-  </body>
-</html>`);
+  res.send(`<!DOCTYPE html>` + );
 });
 
 export default app;
