a4dcec7852072ed0f742511e4a8899bbeff911ff
Ivgeni <ivgenis@gmail.com>
2023-10-17 20:52:25 -0400

    Docu -- menting it!

--- src/server/app.tsx
+++ src/server/app.tsx
@@ -2,10 +2,12 @@ import express from "express";
 import { renderToString } from "react-dom/server";
 import { createElement } from 'react';
 
+import Document from '../components/Document';
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(`<!DOCTYPE html>` + );
+  res.send(`<!DOCTYPE html>` + createElement(Document));
 });
 
 export default app;
