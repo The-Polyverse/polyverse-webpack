e3d8cdc1c1c33e4f3794a24632593e602f8feae5
Ivgeni <ivgenis@gmail.com>
2023-10-17 20:43:12 -0400

    Update document.tsx
    gotta load a client, right?

--- src/components/document.tsx
+++ src/components/document.tsx
@@ -9,6 +9,11 @@ export default function Document() {
       </head>
       <body>
         <div id="root">Hello World!!</div>
+        <script src="https://${
+            process.env.CODESPACE_NAME
+          }-8080.${
+            process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN
+          }/index.js"/>
       </body>
     </html>
   );
