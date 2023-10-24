777edff9cf64eed3e0af3abdce74566a5d587801
Ivgeni <ivgenis@gmail.com>
2023-10-24 02:18:03 +0000

    cleanup

--- src/components/test/box/box.tsx
+++ src/components/test/box/box.tsx
@@ -1,11 +1,33 @@
-import React from "react";
-import { useTheme } from "../../../theme/context";
-import { getStyling } from "../../../utils/styles";
+import React, { FunctionComponent } from "react";
+import {useTheme} from "../../../theme/context";
+import {getStyling} from "../../../utils/styles";
 
-export const Box: React.FunctionComponent<any> = function Box({ rows, cols, rowStart, rowEnd, colStart, colEnd, children, styles }) {
+type BoxProps = {
+  rows?: number;
+  cols?: number;
+  rowStart?: number;
+  rowEnd?: number;
+  colStart?: number;
+  colEnd?: number;
+  styles?: React.CSSProperties;
+  children?: React.ReactNode;
+};
+
+export const Box: FunctionComponent<BoxProps> = function Box(props) {
+  const {
+    rows = 1,
+    cols = 1,
+    rowStart = 1,
+    rowEnd = rows + 1,
+    colStart = 1,
+    colEnd = cols + 1,
+    styles,
+    children
+  } = props;
+
   const theme = useTheme();
-  const { style } = getStyling(theme.box);
-  
+  const {style} = getStyling(theme.box);
+
   const boxStyle = {
     display: 'grid',
     gridTemplateRows: `repeat(${rows}, 1fr)`,
@@ -14,22 +36,24 @@ export const Box: React.FunctionComponent<any> = function Box({ rows, cols, rowS
     gridColumnEnd: colEnd,
     gridRowStart: rowStart,
     gridRowEnd: rowEnd,
-    ...style,
+    ...style
   };
 
 
-  return (
-    <div style={{...boxStyle, ...styles}}>
-      {/* Draw X from corner to corner */}
-      <div style={{
+  return (<div style={
+    {
+      ...boxStyle,
+      ...styles
+    }
+  }> {/* Draw X from corner to corner */}
+    <div style={
+      {
         position: 'absolute',
         top: 0,
         left: 0,
         right: 0,
         bottom: 0,
         background: 'linear-gradient(to top left, transparent 49%, black 49%, black 51%, transparent 51%), linear-gradient(to top right, transparent 49%, black 49%, black 51%, transparent 51%)'
-      }} />
-      {children}
-    </div>
-  );
+      }
+    }/> {children} </div>);
 };
